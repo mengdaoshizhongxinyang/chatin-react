d123a0cdc78f7a539495f6962f8fabf640f50a5c
mengdaoshizhongxinyang <729403918@qq.com>
2021-03-29 18:01:26 +0800

    chore:add ws & cqcode handle

--- src/chatin/index.tsx
+++ src/chatin/index.tsx
@@ -3,15 +3,16 @@
  * @Date: 2021-03-17 11:55:48
  * @Description: 
  */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./chatin.module.less";
 import Login from "../components/login";
 import Bubble from "@/components/bubble";
 import { HTTPRequest } from "@/utils/httpRequest";
-interface BaseMessage{
-  post_type:string
-  time:number
-  self_id:number
+import { WebSocketCollection } from "@/utils/webSocket";
+interface BaseMessage {
+  post_type: string
+  time: number
+  self_id: number
 }
 interface HertType extends BaseMessage {
   interval: number
@@ -20,7 +21,7 @@ interface HertType extends BaseMessage {
   status: { [key in string]: any }
 }
 
-interface Sender{
+interface Sender {
   age: number,
   nickname: string,
   user_id: number
@@ -35,14 +36,17 @@ interface Message extends BaseMessage {
   sub_type: string
   user_id: number
 }
-interface MessageListItem extends Sender{
-  message:string
-  time:number
+interface MessageListItem extends Sender {
+  message: string
+  time: number
 }
+
+let wsc = new WebSocketCollection("get_msg")
+
 const chatin: React.FC<{}> = () => {
   const [content, setContent] = useState<string>("")
   const [state, setState] = useState(0)
-  const [messageList,setMessageList]=useState<MessageListItem[]>([{message:"s",user_id:8786,time:24,nickname:"5245",age:0}])
+  const [messageList, setMessageList] = useState<MessageListItem[]>([{ message: "s", user_id: 8786, time: 24, nickname: "5245", age: 0 }])
   const sendMessage = (message: string) => {
     // if (socket.readyState == WebSocket.OPEN) {
     // 	socket.send(message);
@@ -51,30 +55,42 @@ const chatin: React.FC<{}> = () => {
     // }
   }
   const request = new HTTPRequest('8080')
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key == 'Enter') {
       sendMessage(content)
     }
   }
+  useEffect(() => {
+    // Update the document title using the browser API
+    wsc.register('get_msg', (message) => {
+      const msg: HertType | Message = JSON.parse(message.data)
+      if (msg.post_type == "message") {
 
-  
+        setMessageList(arr => [...arr, {
+          ...msg.sender,
+          time: msg.time,
+          message: msg.message
+        }])
+      }
+    })
+  },[]);
 
-  let socket = new WebSocket("ws://127.0.0.1:6700/get_msg")
-  socket.onmessage = (message: MessageEvent<string>) => {
-    messageList.push({message:"s",user_id:8786,time:24,nickname:"5245",age:0})
-    const msg: HertType | Message = JSON.parse(message.data)
-    // setMessageList(messageList.concat([{message:"s",user_id:8786,time:24,nickname:"5245",age:0}]))
-    if (msg.post_type=="message") {
-      setMessageList(messageList.concat([{
-        ...msg.sender,
-        time:msg.time,
-        message:msg.message
-      }]))
-    }
-  };
-  socket.onopen = function (event) {
-    console.log(event)
-  };
+  // let socket = new WebSocket("ws://127.0.0.1:6700/get_msg")
+  // socket.onmessage = (message: MessageEvent<string>) => {
+  //   messageList.push({message:"s",user_id:8786,time:24,nickname:"5245",age:0})
+  //   const msg: HertType | Message = JSON.parse(message.data)
+  //   // setMessageList(messageList.concat([{message:"s",user_id:8786,time:24,nickname:"5245",age:0}]))
+  //   if (msg.post_type=="message") {
+  //     setMessageList(messageList.concat([{
+  //       ...msg.sender,
+  //       time:msg.time,
+  //       message:msg.message
+  //     }]))
+  //   }
+  // };
+  // socket.onopen = function (event) {
+  //   console.log(event)
+  // };
   async function handleLogin(info: { user: string, password: string }) {
     let result = await request.login(info)
     console.log(result)
@@ -91,10 +107,19 @@ const chatin: React.FC<{}> = () => {
     </div>
     <div className={style['chat-content']}>
       <div className={style['chat-title']}></div>
-      <div className={style['chat-window']} style={{backgroundColor:"rgb(121,157,124)"}}>
+      <div className={style['chat-window']} style={{ backgroundColor: "rgb(121,157,124)" }}>
         {
-          messageList.map((item,index)=>{
-            return <Bubble key={index}>{item.message}</Bubble>
+          messageList.map((item, index) => {
+            return <div
+              className={style['chat-raw']}
+              key={index}
+            >
+              <Bubble
+                time={"12:01PM"}
+                position="right"
+              >{item.message}
+              </Bubble>
+            </div>
           })
         }
       </div>
@@ -102,8 +127,7 @@ const chatin: React.FC<{}> = () => {
         <div className={style['chat-image']}>
           {/* <Icon icon="smile"></Icon> */}
         </div>
-        <input
-          type="text"
+        <textarea
           onKeyPress={handleKeyPress}
           value={content}
           onChange={(e) => setContent(e.target.value)}
--- src/utils/webSocket.ts
+++ src/utils/webSocket.ts
@@ -3,3 +3,43 @@
  * @Date: 2021-03-26 15:51:40
  * @Description: 
  */
+
+declare namespace WS {
+  type Friend={
+    nickname:string
+    remark:string
+    user_id:number
+  }
+  type LoginInfo={
+    name:string
+    user_id:number
+  }
+  type Friends=Friend[]
+  type ReturnMessage<T>={
+    data:T
+    errCode:number
+    status:200|404|500
+  }
+}
+type API='get_msg'|'get_friend_list'
+
+type Func<T=any>=(Message:MessageEvent<string>)=>T
+export class WebSocketCollection<T extends API> {
+  private taskList:{[key in API]?:WebSocket}={}
+  
+  private taskManage:{[key in API]?:Func[]}={}
+  constructor(...startAPIs:T[]){
+    startAPIs.forEach(API=>{
+      this.taskList[API]=new WebSocket(`ws://127.0.0.1:6700/${API}`)
+      this.taskManage[API]=[]
+      this.taskList[API]!.onmessage=(message:MessageEvent<string>)=>{
+        this.taskManage[API]?.forEach(func => {
+          func(message)
+        });
+      }
+    })
+  }
+  register<P=any>(API:T,func:Func<P>){
+    this.taskManage[API]!.push(func)
+  }
+}
