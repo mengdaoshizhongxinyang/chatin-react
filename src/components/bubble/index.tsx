b5deff7277c23c6914923ac4fac8e5cbada37cf4
mengdaoshizhongxinyang <729403918@qq.com>
2021-03-29 17:35:46 +0800

    feat:add bubble like tg

--- src/components/bubble/index.tsx
+++ src/components/bubble/index.tsx
@@ -6,9 +6,20 @@
 import React from "react";
 import style from "./bubble.module.less";
 type Props={
-  content?:React.ReactNode
+  time?:number | string
+  position?:'left'|'right'
+  type?:keyof typeof colorType
+}
+const colorType={
+  primary:style['bubble-primary'],
+  default:""
 }
 const bubble:React.FC<Props>=(props)=>{
-  return <div className={style['bubble-main']}>{props.children || ""}</div>
+  return <div 
+    data-backcolor="#fff" 
+    className={`${style['bubble-main']}  
+    ${props.position=='right'?style['bubble-right']:style['bubble-left']} ${props.type?colorType[props.type]:''}`
+  }>{props.children || ""}<sub className={style['bubble-main-sub']}>{props.time}</sub>
+  </div>
 }
 export default bubble
